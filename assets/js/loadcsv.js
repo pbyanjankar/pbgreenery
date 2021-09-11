b26cf7429840d99c96f93fa1ce7904e27948554e
pbyanjankar <46471995+pbyanjankar@users.noreply.github.com>
2021-09-11 20:48:06 +0545

    Update loadcsv.js

--- assets/js/loadcsv.js
+++ assets/js/loadcsv.js
@@ -1,7 +1,4 @@
-function populateTable(csv) {
-  $.when($.get(csv)).then(
-    function(data){
-      var csvData = $.csv.toArrays(data, {separator: ',', delimiter: '"'});
+function populateTable(csvData) {
 
       $('#parts-table').append("<tbody></tbody>");
 
@@ -19,17 +16,74 @@ function populateTable(csv) {
       }
 
       $("#parts-table").dataTable({
+        "columnDefs": [
+              {
+                  "targets": [3, 4],
+                  "className": 'dt-body-right'
+              }
+        ],
         "columns": [
           { "type": "string" },
           { "type": "string" },
           { "type": "string" },
-          { "type": "num-fmt", render: $.fn.dataTable.render.number( ',', '.', 0 ) },
           { "type": "num-fmt",
-                    render: $.fn.dataTable.render.number( ',', '.', 2 )
+                    render: $.fn.dataTable.render.number( ',', '.', 0 )
+
+          },
+          { "type": "num-fmt",
+                    render: $.fn.dataTable.render.number( ',', '.', 2 ),
+                    
 
           }
         ],
         "paging": true
       });
-    });
+}
+
+function populateFileAge(date1, date2) {
+  try {
+      var diff = getAge( new Date(date1) , new Date(date2) );
+      $('#file-age').append(diff);
+  } catch(err) {
+      return console.log(err.message);
+  }
+}
+
+function getAge(date1, date2) {
+  let milliSec = Math.abs(date1 - date2) / 1000;
+  const days = Math.floor(milliSec / 86400);
+  const lastModified = "Last modified: "
+  
+  if(days>1){
+    return lastModified + days + " days ago"
+  }
+  
+  if(days==1){
+    return lastModified + days + " day ago"
+  }
+
+  milliSec -= days * 86400;
+  const hours = Math.floor(milliSec / 3600) % 24;
+
+  if(hours>1){
+    return lastModified + hours + " hours ago"
+  }
+
+  if(hours==1){
+    return lastModified + hours + " hour ago"
+  }
+
+
+  milliSec -= hours * 3600;
+  const minutes = Math.floor(milliSec / 60) % 60;
+  if(minutes>1){
+    return lastModified + minutes + " minutes ago"
+  }
+
+  if(minutes >= 0 ){
+    return lastModified + minutes + " minute ago"
+  }
+
+
+  return "";
 }
